6ea433aff0cc5ac28cb455f97382584566597c4a
AmjadMoqade98 <amjadmoqade98@gmail.com>
2020-06-22 13:00:21 +0300

    clean the code

--- src/main/java/com/Exalt/MultiThreading/Domain/Dom/ServerDomainService.java
+++ src/main/java/com/Exalt/MultiThreading/Domain/Dom/ServerDomainService.java
@@ -5,8 +5,11 @@ import com.Exalt.MultiThreading.Domain.Dto.CustomerDto;
 import com.Exalt.MultiThreading.Domain.Mapper.ServerMapper;
 import com.Exalt.MultiThreading.Domain.Service.CustomerService;
 import com.Exalt.MultiThreading.Domain.Service.ServerService;
+import com.Exalt.MultiThreading.Runnable.ServerSpan;
 import com.devskiller.friendly_id.FriendlyId;
+import org.apache.tomcat.util.bcel.Const;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -26,28 +29,22 @@ public class ServerDomainService {
     @Autowired
     ServerMapper serverMapper;
 
+    @Autowired
+    ServerSpan serverSpan;
+
     public static HashMap<String, ServerDom> serversLocal = new HashMap<String, ServerDom>();
 
+    enum AllocationMethod {
+        Update, Span
+    }
+
     public void RentServer(String customerId, int space) {
-        boolean flagCreate = false;
-        boolean flagUpdate = false;
-        ServerDom serverDom;
-        synchronized (serversLocal) {
-            String serverId = checkServer(space);
-            if (serverId.equals("-1")) {
-                flagCreate = true;
-                serverId = friendlyId.createFriendlyId();
-                serverDom = new ServerDom();
-                serverDom.setActive(false);
-                serverDom.setId(serverId);
-                serverDom.setRemainingCapacity(Constants.ServerMaximumCapacity - space);
-                serversLocal.put(serverId, serverDom);
-            } else {
-                serverDom = serversLocal.get(serverId);
-                serverDom.setRemainingCapacity(serverDom.getRemainingCapacity() - space);
-                flagUpdate = true;
-            }
-        }
+        AllocationMethod allocationMethod;
+        String serverId = checkAvailableServer(space);
+        allocationMethod = (serverId.equals("-1")) ?
+                AllocationMethod.Span : AllocationMethod.Update;
+
+        ServerDom serverDom = rentLocally(serverId, space, allocationMethod);
 
         // updating customer space
         CustomerDto customerDto = customerService.getCustomer(customerId);
@@ -56,25 +53,49 @@ public class ServerDomainService {
 
 
         // spanning the server
-        if (flagCreate) {
-            serverService.addServer(serverMapper.serverDomToDto(serverDom));
-        } else if (flagUpdate) {
+        if (allocationMethod == AllocationMethod.Span) {
+            serverSpan.setServerDto(serverMapper.serverDomToDto(serverDom));
+            Thread spanThread = new Thread(serverSpan);
+            spanThread.start();
+        }
+        //allocate space in existing server
+        else if (allocationMethod == AllocationMethod.Update) {
             while (!serverDom.isActive()) {
             }
             serverService.updateServer(serverMapper.serverDomToDto(serverDom));
         }
-        flagCreate = false;
-        flagUpdate = false;
     }
 
-    public String checkServer(int targetSpace) {
+    public String checkAvailableServer(int targetSpace) {
         serversLocal = sortByValue(serversLocal);
         for (Map.Entry<String, ServerDom> server : serversLocal.entrySet()) {
             if (server.getValue().getRemainingCapacity() >= targetSpace) {
                 return server.getKey();
             }
         }
+
+        //return -1 if there is no available server with enough space
         return "-1";
+    }
+
+    @Async
+    public ServerDom rentLocally(String serverId, int space, AllocationMethod allocationMethod) {
+        ServerDom serverDom = null;
+        // span new server
+        if (allocationMethod == AllocationMethod.Span) {
+            serverId = friendlyId.createFriendlyId();
+            serverDom = new ServerDom();
+            serverDom.setActive(false);
+            serverDom.setId(serverId);
+            serverDom.setRemainingCapacity(Constants.ServerMaximumCapacity - space);
+            serversLocal.put(serverId, serverDom);
+        }
+        //Allocate space from existing server
+        else if (allocationMethod == AllocationMethod.Update) {
+            serverDom = serversLocal.get(serverId);
+            serverDom.setRemainingCapacity(serverDom.getRemainingCapacity() - space);
+        }
+        return serverDom;
     }
 
     public static HashMap<String, ServerDom> sortByValue(HashMap<String, ServerDom> hm) {
