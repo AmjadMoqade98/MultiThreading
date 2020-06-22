a36319d7fe2ca8b874ceb0512dcffe2a17415a8d
AmjadMoqade98 <amjadmoqade98@gmail.com>
2020-06-22 13:00:47 +0300

    add thread to span the server

---
+++ src/main/java/com/Exalt/MultiThreading/Runnable/ServerSpan.java
@@ -0,0 +1,26 @@
+package com.Exalt.MultiThreading.Runnable;
+
+import com.Exalt.MultiThreading.Domain.Dto.ServerDto;
+import com.Exalt.MultiThreading.Domain.Service.ServerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class ServerSpan implements Runnable{
+
+    @Autowired
+    ServerService serverService ;
+
+    ServerDto serverDto ;
+
+    @Override
+    public void run() {
+        serverService.addServer(serverDto);
+    }
+
+    public void setServerDto(ServerDto serverDto) {
+        this.serverDto = serverDto;
+    }
+}
