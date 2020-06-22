a8db9e15b8395b1efb28f6a95867b4a7bfa9d367
AmjadMoqade98 <amjadmoqade98@gmail.com>
2020-06-22 12:59:32 +0300

    add local data initialization

--- src/main/java/com/Exalt/MultiThreading/MultiThreadingApplication.java
+++ src/main/java/com/Exalt/MultiThreading/MultiThreadingApplication.java
@@ -1,13 +1,35 @@
 package com.Exalt.MultiThreading;
 
+import com.Exalt.MultiThreading.Domain.Dom.ServerDom;
+import com.Exalt.MultiThreading.Domain.Dom.ServerDomainService;
+import com.Exalt.MultiThreading.Domain.Mapper.ServerMapper;
+import com.Exalt.MultiThreading.Infrastructure.Repository.ServerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultiThreadingApplication {
+
+public class MultiThreadingApplication implements CommandLineRunner {
+
+    @Autowired
+    ServerRepository serverRepository ;
+
+    @Autowired
+    ServerMapper serverMapper ;
 
     public static void main(String[] args) {
         SpringApplication.run(MultiThreadingApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        //load the servers from the database to the local storage
+        serverRepository.findAll().forEach(serverDao -> {
+            ServerDom serverDom = serverMapper.serverDaoToDom(serverDao);
+            ServerDomainService.serversLocal.put(serverDom.getId(),serverDom);
+        });
     }
 }
